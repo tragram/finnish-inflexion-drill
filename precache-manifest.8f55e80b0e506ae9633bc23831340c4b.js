58483f7a61f14f41751c96dc188c7299c50d9836
Dominik Hodan <hodandom@fel.cvut.cz>
2020-08-27 17:56:31 +0200

    Updates

---
+++ precache-manifest.8f55e80b0e506ae9633bc23831340c4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91484d91970a222a41293dee08adcaf7",
+    "url": "/finnish-inflexion-drill/index.html"
+  },
+  {
+    "revision": "8ff0a4f49e6c1586676a",
+    "url": "/finnish-inflexion-drill/static/css/main.32556441.chunk.css"
+  },
+  {
+    "revision": "a47eaf3d2bce0eb85f37",
+    "url": "/finnish-inflexion-drill/static/js/2.5402cbe7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/finnish-inflexion-drill/static/js/2.5402cbe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ff0a4f49e6c1586676a",
+    "url": "/finnish-inflexion-drill/static/js/main.14e9223b.chunk.js"
+  },
+  {
+    "revision": "10bffad081f8d88b7a89",
+    "url": "/finnish-inflexion-drill/static/js/runtime-main.f630f979.js"
+  }
+]);
