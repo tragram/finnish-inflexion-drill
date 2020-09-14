ca0a4f98c3cd1fc94bcda0091d6db89eb1bf06b9
Dominik Hodan <domho108@gmail.com>
2020-09-14 21:07:13 +0200

    adding navbar

--- web/src/index.js
+++ web/src/index.js
@@ -5,32 +5,33 @@ import Verbs from './verbs'
 import Notfound from './notfound'
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import { Route, Redirect, NavLink, BrowserRouter as Router, Switch } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
-
-function getTextWidth(inputText, font) {
-  const canvas = document.createElement("canvas");
-  const context = canvas.getContext("2d");
-  context.font = font;
-  const width = context.measureText(inputText).width;
-  const formattedWidth = Math.ceil(width)// + "px"; 
-  console.log(formattedWidth);
-}
-
-const nounPath=process.env.PUBLIC_URL + "/nouns"
-const verbPath=process.env.PUBLIC_URL +"/verbs"
+const nounPath = process.env.PUBLIC_URL + "/nouns"
+const verbPath = process.env.PUBLIC_URL + "/verbs"
 
 const routing = (
   <Router>
     <div>
-      <ul>
-        <li>
-          <NavLink activeClassName="active" to={nounPath}>Nouns</NavLink>
-        </li>
-        <li>
-          <NavLink activeClassName="active" to={verbPath}>Verbs</NavLink>
-        </li>
-      </ul>
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg" >
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">Finnish Inflexion Drill</a>
+        </div>
+        <ul className="nav navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link activeClassName="active" className="nav-link" to={nounPath}>Nouns</Link>
+          </li>
+          <li className="nav-item">
+            <Link activeClassName="active" className="nav-link" to={verbPath}>Verbs</Link>
+          </li>
+        </ul>
+      </nav>
       <Switch>
         <Route exact path={process.env.PUBLIC_URL}>{<Redirect to={nounPath} />}</Route>
         <Route path={nounPath} component={Nouns} />
