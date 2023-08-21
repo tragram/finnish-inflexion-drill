2ce655fe5d18ef1da491d7a37a784e1c33715681
Linus Pauly <pauly.linus@web.de>
2023-08-22 00:07:43 +0200

    Added selection by verbtype ending

--- web/src/word_manager.js
+++ web/src/word_manager.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import UserIO from './userio'
 
+const verb_I_end = ["aa", "ää", "ea", "eä", "ia", "iä", "oa", "ua", "yä", "öä"]
+const verb_II_end = ["da", "dä"]
+const verb_III_end = ["lla", "llä", "nna", "nnä", "rra", "rrä", "sta", "stä"]
+const verb_IV_end = ["ata", "ätä", "ota", "ötä", "uta", "ytä"]
+const verb_V_end = ["ita", "itä"]
+const verb_VI_end = ["eta", "etä"]
+
 class WordManager extends React.Component {
     constructor(props) {
         super(props);
@@ -8,6 +15,8 @@ class WordManager extends React.Component {
         this.kotusWords = props.kotus;
         this.topWordsKeys = Object.keys(this.topWords);
         this.kotusWordsKeys = Object.keys(this.kotusWords);
+        this.mode = props.mode;
+        this.verbgroups = props.enabledVerbgroups;
         this.state = {
             currentWord: "",
             currentAnswer: "",
@@ -35,8 +44,33 @@ class WordManager extends React.Component {
                 data: this.props.top
             };
         }
-        const wordIndex = Math.floor(Math.random() * currentData.keys.length);
-        const word = currentData.keys[wordIndex];
+        let wordIndex;
+        let word;
+
+        this.verbgroups = this.props.enabledVerbgroups;
+
+        while(true) {
+            wordIndex = Math.floor(Math.random() * currentData.keys.length);
+            word = currentData.keys[wordIndex];
+
+            if (this.mode === "verbs") {
+                let ending_two = word.slice(-2);
+                let ending_three = word.slice(-3);
+
+                if(
+                    (this.verbgroups[0] && verb_I_end.includes(ending_two))//valid under this verb group
+                    || (this.verbgroups[1] && verb_II_end.includes(ending_two))
+                    || (this.verbgroups[2] && verb_III_end.includes(ending_three))
+                    || (this.verbgroups[3] && verb_IV_end.includes(ending_three))
+                    || (this.verbgroups[4] && verb_V_end.includes(ending_three))
+                    || (this.verbgroups[5] && verb_VI_end.includes(ending_three))
+                ){
+                    break;
+                }
+            }
+            else break;
+        }
+
         // console.log(this.props.formsOn, this.props.formsOn.map(el => (el && el !== -1 ? el : 0)))
         const formsOnCount = this.props.formsOn.map(el => (el && el !== -1 ? el : 0)).reduce((a, b) => a + b);
         let formSubsetIndex = Math.floor(Math.random() * formsOnCount);
